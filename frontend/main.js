9f500d0a22df2e27029bb3698614528bd97907a3
mihir170901 <mihirchheda17@gmail.com>
2025-03-01 00:15:10 +0530

    created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,18 +2,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
         return response.json()
     }).then(response => {
-        console.log("Website called Function API")
+        console.log("Website called Function API");
         count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error) {
         console.log(error);
-    })
+    });
     return count;
 }
